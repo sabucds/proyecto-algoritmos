bf2889c9cf53f5af3512c96abb2490e4b66d0406
Sabrina Correia <sabrina.correia@correo.unimet.edu.ve>
2021-04-01 15:36:35 -0400

    arregle ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -78,29 +78,28 @@ class Ahorcado(Juego):
             for l in formandose:
                 print(l, end=' ')
             print()
-            print('''1-Ingresar palabra o letra
-2-Usar una pista''')
-            op = ingresar_opcion('una opcion', (1,2))
-            if op == 1:
-                intento = ingresar_alpha('palabra o letra').lower()
-                if intento == self.palabra.lower():
-                    print(f'Es correcto, ganaste: {self.recompensa}')
-                    jugador.guardar_objeto(self.recompensa)
-                    return True
-                elif intento in self.palabra.lower():
-                    for i, letra in enumerate(palabra):
-                        if letra == intento:
-                            formandose[i] = formandose[i].replace('_', letra)
-                            if not '_' in formandose:
-                                print(f'completaste la palabra, ganaste: {self.recompensa}')
-                                jugador.guardar_objeto(self.recompensa.lower())
-                                return True
-                else:
-                    jugador.perder_vida(1/4)
-                    print(f'Incorrecto, pierdes un cuarto de vida. Vidas actuales: {jugador.vidas}')
-                    intentos += 1
-            else:
+            r = input('Ingresa la palabra, una letra  "*" si quieres usar una pista ==> ')
+            if r == "*":
                 p = self.ver_pista_juego(jugador, p)
+            
+            elif r == self.palabra.lower():
+                print(f'Es correcto, ganaste: {self.recompensa}')
+                jugador.guardar_objeto(self.recompensa)
+                return True
+            elif r in self.palabra.lower():
+                for i, letra in enumerate(palabra):
+                    if letra == r:
+                        formandose[i] = formandose[i].replace('_', letra)
+                        if not '_' in formandose:
+                            print(
+                                f'completaste la palabra, ganaste: {self.recompensa}')
+                            jugador.guardar_objeto(self.recompensa.lower())
+                            return True
+            else:
+                jugador.perder_vida(1/4)
+                print(
+                    f'Incorrecto, pierdes un cuarto de vida. Vidas actuales: {jugador.vidas}')
+                intentos += 1
 
 
             print(divisor)
